4f829d57c7b4fb5c0fce2f76b2cadd1e7b84eac6
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-10-22 21:42:45 +0300

    Updates

---
+++ precache-manifest.2ad233c67d565a7de5da0eca8bd1f0dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abca47fd0841db19c64d3b2d9306c46e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85694f7d249e69bdc055",
+    "url": "/portfolio/static/css/main.4808ee84.chunk.css"
+  },
+  {
+    "revision": "22e8dec43014ed0d6d34",
+    "url": "/portfolio/static/js/2.2ec222b6.chunk.js"
+  },
+  {
+    "revision": "bb4c43b27eb8c651f5099d24504f50ed",
+    "url": "/portfolio/static/js/2.2ec222b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85694f7d249e69bdc055",
+    "url": "/portfolio/static/js/main.93619416.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1ee966d656f02e2e41912d7622009854",
+    "url": "/portfolio/static/media/avatar_photo.1ee966d6.png"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/portfolio/static/media/github.b6ec19ce.png"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "/portfolio/static/media/instagram.8290266a.png"
+  },
+  {
+    "revision": "65ae285ae55fdf96bfb1cd0427f28523",
+    "url": "/portfolio/static/media/landing2.65ae285a.jpeg"
+  },
+  {
+    "revision": "b37d6df3b36a74748a9bda890b3ba550",
+    "url": "/portfolio/static/media/telegram.b37d6df3.png"
+  }
+]);
